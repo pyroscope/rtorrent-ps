d00e5f7dde11e7025350ecbfea8bfceffbb0363f
pyroscope <pyroscope.project@gmail.com>
2011-05-03 03:12:18 +0000

    added ui.bind_key

--- patches/command_pyroscope.cc
+++ patches/command_pyroscope.cc
@@ -16,6 +16,7 @@
 // 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 #include "config.h"
+#include "globals.h"
 
 #include <ctime>
 #include <rak/functional.h>
@@ -26,6 +27,8 @@
 #include "core/view_manager.h"
 #include "ui/root.h"
 #include "ui/download_list.h"
+#include "ui/element_base.h"
+#include "ui/element_download_list.h"
 #include "rpc/command_slot.h"
 #include "rpc/command_variable.h"
 #include "rpc/parse.h"
@@ -107,7 +110,62 @@ torrent::Object apply_compare(rpc::target_type target, const torrent::Object& ra
     return (int64_t) (target.second < target.third);
 }
 
+
+static std::map<char, std::string> bound_commands[ui::DownloadList::DISPLAY_MAX_SIZE];
+
+/*  @DOC
+    ui.bind_key=display,key,"command1=[,...]"
+
+    Binds the given key on a specified display to execute the commands when pressed.
+    
+    "display" must be one of "download_list", ...
+    "key" can be either a single character for normal keys, or ^ plus a character for control keys.
+*/
+torrent::Object apply_ui_bind_key(const torrent::Object& rawArgs) {
+    const torrent::Object::list_type& args = rawArgs.as_list();
+
+    if (args.size() != 3)
+        throw torrent::input_error("Expecting display, key, and commands.");
+
+    // Parse positional arguments
+    torrent::Object::list_const_iterator itr = args.begin();
+    const std::string& element  = (itr++)->as_string();
+    const std::string& keydef   = (itr++)->as_string();
+    const std::string& commands = (itr++)->as_string();
+
+    // Get key index from definition    
+    if (keydef.empty() || keydef.size() > (keydef[0] == '^' ? 2 : 1))
+        throw torrent::input_error("Bad key definition.");
+    char key = keydef[0];
+    if (key == '^' && keydef.size() > 1) key = keydef[1] & 31;
+
+    // Look up display    
+    ui::DownloadList::Display displayType = ui::DownloadList::DISPLAY_MAX_SIZE;
+    if (element == "download_list") {
+        displayType = ui::DownloadList::DISPLAY_DOWNLOAD_LIST;
+    } else {
+        throw torrent::input_error(std::string("Unknown element ") + element);
+    }
+    ui::ElementBase* display = control->ui()->download_list()->display(displayType);
+    if (!display)
+        throw torrent::input_error("Display not found.");
+
+    // Bind the key to the given commands
+    bound_commands[displayType][key] = commands; // keep hold of the string, so the c_str() below remains valid
+    switch (displayType) {
+        case ui::DownloadList::DISPLAY_DOWNLOAD_LIST:
+            display->bindings()[key] = sigc::bind(sigc::mem_fun(
+                *(ui::ElementDownloadList*)display, &ui::ElementDownloadList::receive_command), bound_commands[displayType][key].c_str());
+            break;
+    }
+
+    return torrent::Object();
+}
+
+
+
 void initialize_command_pyroscope() {
     ADD_ANY_LIST("compare", rak::ptr_fn(&apply_compare));
+    ADD_COMMAND_LIST("ui.bind_key", rak::ptr_fn(&apply_ui_bind_key));
 }
 
