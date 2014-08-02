06dd553b95a767325abf59c05499d6723ea4638e
pyroscope <pyroscope.project@gmail.com>
2014-08-02 11:44:19 +0200

    use _cxxstd_ define consistently

--- patches/command_pyroscope.cc
+++ patches/command_pyroscope.cc
@@ -243,7 +243,7 @@ torrent::Object apply_ui_bind_key(const torrent::Object& rawArgs) {
 #if RT_HEX_VERSION < 0x000904
                 sigc::bind(sigc::mem_fun(*(ui::ElementDownloadList*)display, &ui::ElementDownloadList::receive_command),
 #else
-                std::tr1::bind(&ui::ElementDownloadList::receive_command, (ui::ElementDownloadList*)display,
+                _cxxstd_::bind(&ui::ElementDownloadList::receive_command, (ui::ElementDownloadList*)display,
 #endif
                 bound_commands[displayType][key].c_str());
             break;
