fd83c7481c4c8b441c588410e723b319df06c970
pyroscope <pyroscope.project@gmail.com>
2018-05-26 10:16:59 +0200

    bintray: add newline after Bintray JSON response

--- bintray.sh
+++ bintray.sh
@@ -7,6 +7,6 @@ vers=$(cut -f2 -d_ <<<"$file")
 vers=${vers%%~*}
 url="https://api.bintray.com/content/pyroscope/rtorrent-ps/rtorrent-ps/$vers/$file"
 
-echo "Uploading to $url..." 
+echo "Uploading to $url..."
 curl -# -T "$path" -u$BINTRAY_ACCOUNT:$BINTRAY_API_KEY "$url"
-
+echo
