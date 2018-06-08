423dd86c5e4b08beb268027f6bf02ff2aa769e36
pyroscope <pyroscope.project@gmail.com>
2018-06-08 20:30:20 +0200

    managing visibility of canvas columns (#86)

--- patches/ui_pyroscope.cc
+++ patches/ui_pyroscope.cc
@@ -19,6 +19,7 @@ python -c 'print u"\u22c5 \u22c5\u22c5 \u201d \u2019 \u266f \u2622 \u260d \u2318
 
 #include <cstdio>
 #include <cwchar>
+#include <set>
 #include <list>
 #include <stdlib.h>
 #include <unistd.h>
@@ -46,6 +47,7 @@ python -c 'print u"\u22c5 \u22c5\u22c5 \u201d \u2019 \u266f \u2622 \u260d \u2318
 
 #define D_INFO(item) (item->info())
 #include "rpc/object_storage.h"
+#include "rpc/parse.h"
 
 // from command_pyroscope.cc
 extern torrent::Tracker* get_active_tracker(torrent::Download* item);
@@ -174,8 +176,19 @@ std::string get_custom_string(core::Download* d, const char* name) {
 }
 
 
+// get a value from arg, either parsing from a string, or arg already being a value
+int64_t parse_value_arg(const torrent::Object& arg) {
+    if (arg.is_string()) {
+        int64_t result;
+        rpc::parse_whole_value(arg.as_string().c_str(), &result);
+        return result;
+    }
+    return arg.as_value();  // this will throw if other types than string/value are passed
+}
+
+
 // convert absolute timestamp to approximate human readable time diff (5 chars wide)
-std::string elapsed_time(unsigned long dt, unsigned long t0)  {
+std::string elapsed_time(unsigned long dt, unsigned long t0) {
     if (dt == 0) return std::string("⋆ ⋆⋆ ");
 
     const char* unit[] = {"”", "’", "h", "d", "w", "m", "y"};
@@ -220,6 +233,9 @@ std::string num2(int64_t num) {
 
 
 namespace display {
+
+// Visibility of canvas columns
+static std::set<int> column_hidden;
 
 
 // function wrapper for what possibly is a macro
@@ -556,6 +572,48 @@ void ui_pyroscope_download_list_redraw_item(Window* window, display::Canvas* can
         canvas->set_attr(1, pos+1, 1, attr_map[ps::COL_ALARM + offset], ps::COL_ALARM + offset);
         canvas->set_attr(1, pos+2, -1, attr_map[ps::COL_ALARM + offset], ps::COL_ALARM + offset);
     }
+}
+
+
+torrent::Object ui_column_hide(rpc::target_type target, const torrent::Object::list_type& args) {
+    for(torrent::Object::list_const_iterator itr = args.begin(), last = args.end(); itr != last; ++itr) {
+        int64_t colidx = parse_value_arg(*itr);
+        column_hidden.insert(colidx);
+    }
+
+    return torrent::Object();
+}
+
+
+torrent::Object ui_column_show(rpc::target_type target, const torrent::Object::list_type& args) {
+    for(torrent::Object::list_const_iterator itr = args.begin(), last = args.end(); itr != last; ++itr) {
+        int64_t colidx = parse_value_arg(*itr);
+        column_hidden.erase(colidx);
+    }
+
+    return torrent::Object();
+}
+
+
+torrent::Object ui_column_is_hidden(rpc::target_type target, const torrent::Object::list_type& args) {
+    if (args.size() != 1) {
+        throw torrent::input_error("ui.column.is_hidden takes exactly one argument!");
+    }
+    int64_t colidx = parse_value_arg(*args.begin());
+
+    return (int64_t) column_hidden.count(colidx);
+}
+
+
+torrent::Object ui_column_hidden_list() {
+    torrent::Object result = torrent::Object::create_list();
+    torrent::Object::list_type& resultList = result.as_list();
+
+    for (std::set<int>::const_iterator itr = column_hidden.begin(); itr != column_hidden.end(); itr++) {
+       resultList.push_back(*itr);
+    }
+
+    return result;
 }
 
 
@@ -978,6 +1036,11 @@ void initialize_command_ui_pyroscope() {
 
     CMD2_ANY        ("ui.canvas_color",         _cxxstd_::bind(&display::ui_canvas_color_get));
     CMD2_ANY_STRING ("ui.canvas_color.set",     _cxxstd_::bind(&display::ui_canvas_color_set, _cxxstd_::placeholders::_2));
+
+    CMD2_ANY_LIST("ui.column.hide", &display::ui_column_hide);
+    CMD2_ANY_LIST("ui.column.show", &display::ui_column_show);
+    CMD2_ANY_LIST("ui.column.is_hidden", &display::ui_column_is_hidden);
+    CMD2_ANY("ui.column.hidden.list", _cxxstd_::bind(&display::ui_column_hidden_list));
 
     PS_VARIABLE_COLOR("ui.color.progress0",     "red");
     PS_VARIABLE_COLOR("ui.color.progress20",    "bold bright red");
