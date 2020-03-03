16a6a11ef5b69c64affdc29c97b0ec8f52a9e5f2
pyroscope <pyroscope.project@gmail.com>
2020-03-03 20:21:46 +0100

    boostrap: optimize venv creation

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,15 +9,15 @@ VENV_NAME="rtorrent-ps"
 SCRIPTNAME="$0"
 test "$SCRIPTNAME" != "-bash" -a "$SCRIPTNAME" != "-/bin/bash" || SCRIPTNAME="${BASH_SOURCE[0]}"
 
-test ! -x "$1" || VIRTUALENV="$1"
+test -z "$1" || PYTHON="$1"
 
 deactivate 2>/dev/null || true
 test -n "$VENV_NAME" || VENV_NAME="$(basename $(builtin cd $(dirname "$SCRIPTNAME") && pwd))"
-test -x ".pyvenv/$VENV_NAME/bin/python" || ${PYVENV:-/usr/bin/pyvenv} ".pyvenv/$VENV_NAME"
-. ".pyvenv/$VENV_NAME/bin/activate"
+test -x ".venv/bin/python" || ${PYTHON:-python3} -m venv --prompt "$VENV_NAME" ".venv"
+. ".venv/bin/activate"
 
 for basepkg in pip setuptools wheel; do
-    pip install -U $basepkg
+    .venv/bin/pip install -U $basepkg
 done
 
-pip install -r docs/requirements.txt
+.venv/bin/pip install -r docs/requirements.txt
